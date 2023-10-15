637b4f0e56f9b7db002693e0de335351e85eff58
KOUBELANRICHARD <richard.akpagni@gmail.com>
2023-10-15 06:10:14 +0200

    Crréation des applications listings et blog

---
+++ listings/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Bienvenue nous allons parler de nous.')
+
+def listings(request):
+    return HttpResponse('<h1>Cette page présente la liste des annonces<h1>')
+
+def contact_us(request):
+    return HttpResponse('<h1>Bienvenue sur notre page de contact')
+
+def index(request):
+    return render(request, "index.html")
+
--- website/settings.py
+++ website/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR),'listings/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
